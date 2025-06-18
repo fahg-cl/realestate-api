c8ca40a27215c2c1d722498b4b1e864d818b231d
Fabián Hernández <fahg76@gmail.com>
2025-06-17 21:48:58 -0400

    Push 3

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 const inmuebles = [
-	{ id: 1, direccion: 'Calle 123', precio: 150000, tipo: 'Apartamento' },
-	{ id: 2, direccion: 'Avenida 456', precio: 230000, tipo: 'Casa' },
-	{ id: 3, direccion: 'Carrera 789', precio: 120000, tipo: 'Estudio' }
+	{ id: 1, direccion: 'Calle 123_1', precio: 150000, tipo: 'Apartamento' },
+	{ id: 2, direccion: 'Avenida 456_1', precio: 230000, tipo: 'Casa' },
+	{ id: 3, direccion: 'Carrera 789_1', precio: 120000, tipo: 'Estudio' }
 ];
 
 app.get('/api/inmuebles', (req, res) => {
